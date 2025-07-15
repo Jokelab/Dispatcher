fb2a77dfab270fb432632e92938ed7ea226653df
Robert Beisicht <robert@beisicht.nl>
2025-07-15 15:01:31 +0200

    Cleanup benchmark program.cs

--- test/Dispatcher.Benchmark/Program.cs
+++ test/Dispatcher.Benchmark/Program.cs
@@ -1,12 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using BenchmarkDotNet.Running;
-using Dispatcher;
+﻿using BenchmarkDotNet.Running;
 using Dispatcher.Benchmark;
-using Dispatcher.Extensions;
-using MediatR;
-using Microsoft.Extensions.DependencyInjection;
-
-
-
 
 BenchmarkRunner.Run<RequestHandlerBenchmark>();
